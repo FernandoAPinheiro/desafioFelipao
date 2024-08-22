80225c6e6fccdadf8d39783c8d3adc95e7e232e6
FernandoAPinheiro <fernandodeaquinopinheiro@gmail.com>
2024-08-21 23:30:12 -0300

    Desafio concluido

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//Classificador de nivel de heroi
+let nome = "Fernando";
+let xp= 9000;
+let nivel ="";
+
+if(xp <= 1000){
+    nivel ="Ferro";
+}else if( xp >1000 && xp <=2000){
+    nivel = "Bronze";
+}else if( xp >2000 && xp <=6000){
+    nivel = "Prata";
+}else if( xp >6000 && xp <=7000){
+    nivel = "Ouro";
+}else if( xp >7000 && xp <=8000){
+    nivel = "Platina";
+}else if( xp >8000 && xp <=9000){
+    nivel = "Ascendente";
+}else if( xp >9000 && xp <=10000){
+    nivel = "Imortal";
+}else{
+    nivel = "Radiante";
+}
+
+console.log(`O heroi de nome ${nome}, está no nivel ${nivel}`)
+
